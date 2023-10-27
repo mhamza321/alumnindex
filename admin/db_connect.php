a828fe5eca3b8d6f9260f77384d037e3df6f8c91
mhamza321 <hk246337@gmail.com>
2023-10-27 11:03:00 +0500

    user name password updated for online portal

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '', 'alumni_portal_db') or die("Could not connect to mysql" . mysqli_error($con));
+$conn = new mysqli('localhost', 'admin', 'MyN3wP4ssw0rd', 'alumni_portal_db') or die("Could not connect to mysql" . mysqli_error($con));
