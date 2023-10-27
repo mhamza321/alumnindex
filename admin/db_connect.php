10c4cab2b53e2bc9102027d30fadcddc3eb6a301
mhamza321 <hk246337@gmail.com>
2023-10-27 13:43:41 +0500

    testing 4

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,5 @@
 <?php
-
+// For online access
 $conn = new mysqli('localhost', 'admin', 'MyN3wP4ssw0rd', 'alumni_portal_db') or die("Could not connect to mysql" . mysqli_error($con));
+// For offline
+//$conn = new mysqli('localhost', 'root', '', 'alumni_portal_db') or die("Could not connect to mysql" . mysqli_error($con));
